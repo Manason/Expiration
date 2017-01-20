f23e12a064863bf56c86dd1f2ed4bcb9dcc1c695
Brett Case <brett.case97@gmail.com>
2017-01-20 14:52:57 -0800

    Reinserted SQL code

--- fill_db.php
+++ fill_db.php
@@ -11,8 +11,8 @@ $username = USERNAME;
 $password = PASSWORD;
 $dbname = DATABASE_NAME;
 
-$test1 = $_POST["food"];
-$test2 = $_POST["expiration"];
+$food = $_POST["food"];
+$exp_date = $_POST["expiration"];
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
@@ -21,16 +21,15 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-echo $test1;
-echo $test2;
+echo $food;
+echo $exp_date;
 
-/*$sql = "INSERT INTO " . $dbname . " (name, exp_date) VALUES ($_POST('food'), $_POST('expiration'))";
+$sql = "INSERT INTO " . $dbname . " (name, exp_date) VALUES ($food, $exp_date)";
 
 if (mysqli_query($conn, $sql)) {
     echo "New record created successfully";
 } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
-*/
 mysqli_close($conn);
 ?>
