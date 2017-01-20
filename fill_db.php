3af1ba4fb3c3c6772f578d628da0255113fbaff2
Brett Case <brett.case97@gmail.com>
2017-01-20 14:33:15 -0800

    fix testing code

--- fill_db.php
+++ fill_db.php
@@ -11,8 +11,8 @@ $username = USERNAME;
 $password = PASSWORD;
 $dbname = DATABASE_NAME;
 
-$test1 = "$_POST('food')";
-$test2 = "$_POST('expiration')";
+$test1 = $_POST('food');
+$test2 = $_POST('expiration');
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
@@ -21,16 +21,16 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-echo $test1;
-echo $test2;
+echo "$test1";
+echo "$test2";
 
-$sql = "INSERT INTO " . $dbname . " (name, exp_date) VALUES ($_POST('food'), $_POST('expiration'))";
+/*$sql = "INSERT INTO " . $dbname . " (name, exp_date) VALUES ($_POST('food'), $_POST('expiration'))";
 
 if (mysqli_query($conn, $sql)) {
     echo "New record created successfully";
 } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
-
+*/
 mysqli_close($conn);
 ?>
