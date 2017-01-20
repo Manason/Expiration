ef5a0b6946aa18947ea0ca96bfe76d8cfa528a9f
Brett Case <brett.case97@gmail.com>
2017-01-20 14:27:15 -0800

    Added testing code

--- fill_db.php
+++ fill_db.php
@@ -11,12 +11,18 @@ $username = USERNAME;
 $password = PASSWORD;
 $dbname = DATABASE_NAME;
 
+$test1 = $_POST('food');
+$test2 = $_POST('expiration');
+
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
+
+echo $test1;
+echo $test2;
 
 $sql = "INSERT INTO " . $dbname . " (name, exp_date) VALUES ($_POST('food'), $_POST('expiration'))";
 
