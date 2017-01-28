ceda8ef685d0f8f696ca28515e0fee7bf0479cfc
Brett Case <brett.case97@gmail.com>
2017-01-27 19:33:09 -0800

    cleaning up comments and centering calendar

--- fill_db.php
+++ fill_db.php
@@ -15,10 +15,6 @@ $tablename = "Food";
 $food = $_POST["food"];
 $exp_date = $_POST["expiration"];
 
-echo "Displaying post results of food and exp_date";
-echo $food;
-echo $exp_date;
-
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
