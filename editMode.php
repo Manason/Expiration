31899b250546350ca0d617596716c2ca9bf4d8a7
Manason <clanerp@gmail.com>
2017-01-30 15:57:42 -0800

    began the frontend of edit mode

---
+++ editMode.php
@@ -0,0 +1,45 @@
+<?php
+
+if(file_exists('db-config.php')){
+	require_once 'db-config.php';
+}
+else{
+	die("Error: db-config.php not found");
+}
+$servername = SERVER_NAME;
+$username = USERNAME;
+$password = PASSWORD;
+$dbname = DATABASE_NAME;
+$tablename = "Food";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT name, exp_date FROM " . $tablename;
+$result = mysqli_query($conn, $sql);
+
+echo
+"<table align=\"center\">
+<tr>
+<th>Name</th>
+<th>Expiration Date</th>
+<th>Delete Entry</th>
+</tr>";
+
+while($row = mysqli_fetch_array($result)){
+	echo
+	"<tr>
+	 <td><input type=\"text\">" . $row['name'] . "</input></td>
+     <td>" . $row['exp_date'] . "</td>
+	 <td><input type=\"checkbox\"></input></td>
+	 </tr>";
+
+}
+
+echo "</table>";
+mysqli_close($conn);
