9fa43dd77951a71c7ec4aa9416d786148575d45d
Brett Case <brett.case97@gmail.com>
2017-01-30 17:05:45 -0800

    Worked on sending email
    currently has issue calling script

--- sendEmail.php
+++ sendEmail.php
@@ -12,9 +12,6 @@ $password = PASSWORD;
 $dbname = DATABASE_NAME;
 $tablename = "Food";
 
-//get stuff for email
-$visitor_email = EMAIL;//$_POST['email'];
-
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
@@ -22,17 +19,18 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-// $food = "SELECT name FROM ". tablename . "WHERE exp_date = CURDATE()";
+//make food the items that expire today separated by a comma
 $food = "SELECT GROUP_CONCAT(name) FROM ".tablename." WHERE exp_date = CURDATE()"
-$result = mysqli_query($conn, $food);
-echo $food;
+echo $food; //see what is going to be sent
 
-// $email_from = 'yourname@yourwebsite.com';
-// $email_subject = $food . "About To Expire!";
-// $email_body = "Here are some recipes to use!";
-// $headers = "From: $email_from \r\n";
-// $headers .= "Reply-To: $visitor_email \r\n";
-//
+$visitor_email = EMAIL; //address the email will be sent to
+$email_from = 'yourname@yourwebsite.com'; //address the email is from
+$email_subject = $food . "About To Expire!"; //subject of email with food added in
+$email_body = "Here are some recipes to use!"; //body of email
+$headers = "From: $email_from \r\n"; //headers for email
+$headers .= "Reply-To: $visitor_email \r\n"; //more headers
+
+//send email (send to, subject, body, headers)
 // mail($visitor_email,$email_subject,$email_body,$headers)
 
 mysqli_close($conn);
