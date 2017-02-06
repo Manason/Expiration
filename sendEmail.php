2f4995c7f50f1bdc4cc2ed46ab32a6cd524d1aa0
Brett Case <brett.case97@gmail.com>
2017-02-06 15:14:14 -0800

    testing to see what will be sent in email

--- sendEmail.php
+++ sendEmail.php
@@ -20,15 +20,16 @@ if (!$conn) {
 }
 
 //make food the items that expire today separated by a comma
-$food = "SELECT GROUP_CONCAT(name) FROM ".tablename." WHERE exp_date = CURDATE()"
+$food = "SELECT * FROM " . tablename;
+// $food = "SELECT GROUP_CONCAT(name) FROM ".tablename." WHERE exp_date = CURDATE()"
 echo $food; //see what is going to be sent
 
-$visitor_email = EMAIL; //address the email will be sent to
-$email_from = 'yourname@yourwebsite.com'; //address the email is from
-$email_subject = $food . "About To Expire!"; //subject of email with food added in
-$email_body = "Here are some recipes to use!"; //body of email
-$headers = "From: $email_from \r\n"; //headers for email
-$headers .= "Reply-To: $visitor_email \r\n"; //more headers
+// $visitor_email = EMAIL; //address the email will be sent to
+// $email_from = 'yourname@yourwebsite.com'; //address the email is from
+// $email_subject = $food . "About To Expire!"; //subject of email with food added in
+// $email_body = "Here are some recipes to use!"; //body of email
+// $headers = "From: $email_from \r\n"; //headers for email
+// $headers .= "Reply-To: $visitor_email \r\n"; //more headers
 
 //send email (send to, subject, body, headers)
 // mail($visitor_email,$email_subject,$email_body,$headers)
