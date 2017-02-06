f46d1dc261917758afb9566311dbe583480f4edb
Brett Case <brett.case97@gmail.com>
2017-02-06 15:52:56 -0800

    Bug fixes

--- sendEmail.php
+++ sendEmail.php
@@ -1,41 +1,38 @@
 <?php
-
 if(file_exists('db-config.php')){
 	require_once 'db-config.php';
 }
 else{
 	die("Error: db-config.php not found");
 }
+
 $servername = SERVER_NAME;
 $username = USERNAME;
 $password = PASSWORD;
 $dbname = DATABASE_NAME;
 $tablename = "Food";
-
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
-
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-//make food the items that expire today separated by a comma
-// $food = "SELECT GROUP_CONCAT(name) FROM ".tablename." WHERE exp_date = CURDATE()"
-
-$sql = "SELECT name FROM" . $tablename;
+$sql = "SELECT id, firstname, lastname FROM MyGuests";
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
-        echo " - Name: " . $row["name"]. "<br>";
+        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
     }
 } else {
     echo "0 results";
 }
 
 
+
+// $food = "SELECT GROUP_CONCAT(name) FROM ".tablename." WHERE exp_date = CURDATE()"
 // $visitor_email = EMAIL; //address the email will be sent to
 // $email_from = 'yourname@yourwebsite.com'; //address the email is from
 // $email_subject = $food . "About To Expire!"; //subject of email with food added in
