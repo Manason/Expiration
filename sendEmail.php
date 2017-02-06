a381887895d441e5a136ca7fdef63aef6450020f
Brett Case <brett.case97@gmail.com>
2017-02-06 15:55:49 -0800

    bug fixes
    made table visible

--- sendEmail.php
+++ sendEmail.php
@@ -18,13 +18,13 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$sql = "SELECT name FROM" . $tablename;
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
-        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+        echo "Name: " . $row["name"]. "<br>";
     }
 } else {
     echo "0 results";
