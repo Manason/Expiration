776ef4944eae461a6de293262b61aa32a690359c
Manason <clanerp@gmail.com>
2017-02-20 17:02:19 -0800

    deleting the table right after user clicks save changes

--- deleteTable.php
+++ deleteTable.php
@@ -20,29 +20,7 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-$sql = "SELECT id, name, exp_date FROM " . $tablename;
+$sql = "TRUNCATE TABLE " . $tablename;
 $result = mysqli_query($conn, $sql);
-
-echo
-"<table align=\"center\">
-<tr>
-<th>ID</th>
-<th>Name</th>
-<th>Date</th>
-<th>Delete Entry</th>
-</tr>";
-
-while($row = mysqli_fetch_array($result)){
-	echo
-	"<tr>
-	 <td>" . $row['id'] . "</td>
-	 <td><input type=\"text\" value=\"". $row['name'] ."\"</input></td>
-     <td>" . $row['exp_date'] . "</td>
-	 <td><input type=\"checkbox\"></input></td>
-	 </tr>";
-
-}
-
-echo "</table>";
 
 mysqli_close($conn);
