fe0ada09f9e80f6ac0ba811a4eff7982c3074f7b
Manason <clanerp@gmail.com>
2017-03-13 16:47:40 -0700

    no default checked

--- editMode.php
+++ editMode.php
@@ -38,7 +38,7 @@ while($row = mysqli_fetch_array($result)){
 	 <td>" . $row['id'] . "</td>
 	 <td><input type=\"text\" value=\"". $row['name'] ."\"</input></td>
      <td>" . $row['exp_date'] . "</td>
-	 <td><input type=\"checkbox\" checked=\"false\"></input></td>
+	 <td><input type=\"checkbox\"></input></td>
 	 </tr>";
 
 }
